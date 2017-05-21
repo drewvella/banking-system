57a6e4af43d16d0a3c34d565e0f2ab64c94b9665
Andrew Vella <andrewvella@Andrews-MBP.lan>
2017-05-21 02:54:31 +0200

    Reverted queue cleanups

--- logging-system/src/main/java/com/demo/logging/service/LoggingConfig.java
+++ logging-system/src/main/java/com/demo/logging/service/LoggingConfig.java
@@ -1,6 +1,11 @@
 package com.demo.logging.service;
 
 import com.google.gson.Gson;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +16,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class LoggingConfig {
 
+    @Value("${spring.rabbitmq.queuename}")
+    private String queueName;
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange("spring-boot-exchange");
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(queueName);
+    }
 
     @Bean
     Gson gson() {
